168e2680e369a37be344af52da907916380b97c0
ozgunyargi <ozgun.yargi@gmail.com>
2024-01-14 15:19:38 +0300

    Add config.py and requirements.txt files

---
+++ config.py
@@ -0,0 +1,12 @@
+mtcnn_params = {
+    "image_size": 160,
+    "margin": 0,
+    "min_face_size": 20,
+    "thresholds": [0.6, 0.7, 0.7],
+    "factor": 0.709,
+    "post_process": True,
+    "select_largest": True,
+    "selection_method": None,
+    "keep_all": False,
+    "device": None
+}
